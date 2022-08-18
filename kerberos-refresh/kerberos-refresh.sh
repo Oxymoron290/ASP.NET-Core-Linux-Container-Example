f5242367cd33dd1b272b3dd4afd20bf600f864d0
Timothy Sturm <timothysturm@microsoft.com>
2022-08-17 22:31:20 -0500

    #1 - potential solution to ensure authentication before startup

--- kerberos-refresh/kerberos-refresh.sh
+++ kerberos-refresh/kerberos-refresh.sh
@@ -3,10 +3,8 @@
 [[ "$PERIOD_SECONDS" == "" ]] && PERIOD_SECONDS=3600
 
 if [[ "$OPTIONS" == "" ]]; then
-
   [[ -e /krb5/krb5.keytab ]] && OPTIONS="-k" && echo "*** using host keytab"
   [[ -e /krb5/client.keytab ]] && OPTIONS="-k -i" && echo "*** using client keytab"
-
 fi
 
 if [[ -z "$(ls -A /krb5)" ]]; then
@@ -16,9 +14,13 @@ fi
 while true
 do
   echo "*** kinit at " + $(date -I)
-   kinit -V $OPTIONS $APPEND_OPTIONS
-   klist -c /dev/shm/ccache 
-   echo "*** Waiting for $PERIOD_SECONDS seconds"
-   sleep $PERIOD_SECONDS
-   
+  kinit -V $OPTIONS $APPEND_OPTIONS
+  klist -c /dev/shm/ccache 
+  if [[ -z "$INIT_ONLY" ]]; then
+    if [ klist -s ]; then
+      exit 0;
+    fi
+  fi
+  echo "*** Waiting for $PERIOD_SECONDS seconds"
+  sleep $PERIOD_SECONDS 
 done
